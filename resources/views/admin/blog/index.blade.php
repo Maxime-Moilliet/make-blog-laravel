176edb72cf7e75831dae3109b0016b8d41064f41
Maxime Mllt <mlltmax66@gmail.com>
2024-04-19 19:19:00 +0200

    ab pan admin list posts

--- resources/views/admin/blog/index.blade.php
+++ resources/views/admin/blog/index.blade.php
@@ -17,49 +17,144 @@
         </div>
     </div>
 
-    <section class="card">
-        <div class="card-header">
-            <h2 class="card-title">Posts</h2>
+    <ul class="nav nav-pills pb-2" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="draft-tab" data-bs-toggle="tab" data-bs-target="#draft-tab-pane"
+                    type="button" role="tab" aria-controls="draft-tab-pane" aria-selected="true">Draft
+            </button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="published-tab" data-bs-toggle="tab" data-bs-target="#published-tab-pane"
+                    type="button" role="tab" aria-controls="published-tab-pane" aria-selected="false">Published
+            </button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="archived-tab" data-bs-toggle="tab" data-bs-target="#archived-tab-pane"
+                    type="button" role="tab" aria-controls="archived-tab-pane" aria-selected="false">Archived
+            </button>
+        </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="draft-tab-pane" role="tabpanel" aria-labelledby="draft-tab"
+             tabindex="0">
+            <section class="card">
+                <div class="card-body">
+                    <table id="draftTable" class="display w-100">
+                        <thead>
+                        <tr>
+                            <th>Action</th>
+                            <th>Title</th>
+                            <th>Slug</th>
+                            <th>CreatedAt</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($posts_draft as $post_draft)
+                            <tr>
+                                <td>
+                                    <div class="d-flex gap-2">
+                                        <a href="{{ route('blog.show', $post_draft->slug) }}"
+                                           class="btn btn-sm btn-primary">
+                                            Show
+                                        </a>
+                                        <a href="{{ route('admin.blog.edit', $post_draft) }}"
+                                           class="btn btn-sm btn-warning">
+                                            Edit
+                                        </a>
+                                    </div>
+                                </td>
+                                <td>{{ $post_draft->title }}</td>
+                                <td>{{ $post_draft->slug }}</td>
+                                <td>{{ $post_draft->created_at->format('d/m/Y à h:i') }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </section>
         </div>
-        <div class="card-body">
-            <table id="myTable" class="display">
-                <thead>
-                <tr>
-                    <th>Action</th>
-                    <th>Title</th>
-                    <th>Slug</th>
-                    <th>Status</th>
-                    <th>CreatedAt</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($posts as $post)
-                    <tr>
-                        <td>
-                            <div class="d-flex gap-2">
-                                <a href="{{ route('blog.show', $post->slug) }}" class="btn btn-sm btn-primary">
-                                    Show
-                                </a>
-                                <a href="{{ route('admin.blog.edit', $post) }}" class="btn btn-sm btn-warning">
-                                    Edit
-                                </a>
-                            </div>
-                        </td>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->slug }}</td>
-                        <td>{{ $post->status }}</td>
-                        <td>{{ $post->created_at->format('d/m/Y à h:i') }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+        <div class="tab-pane fade" id="published-tab-pane" role="tabpanel" aria-labelledby="published-tab" tabindex="0">
+            <section class="card">
+                <div class="card-body">
+                    <table id="publishedTable" class="display w-100">
+                        <thead>
+                        <tr>
+                            <th>Action</th>
+                            <th>Title</th>
+                            <th>Slug</th>
+                            <th>CreatedAt</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($posts_published as $post_published)
+                            <tr>
+                                <td>
+                                    <div class="d-flex gap-2">
+                                        <a href="{{ route('blog.show', $post_published->slug) }}"
+                                           class="btn btn-sm btn-primary">
+                                            Show
+                                        </a>
+                                        <a href="{{ route('admin.blog.edit', $post_published) }}"
+                                           class="btn btn-sm btn-warning">
+                                            Edit
+                                        </a>
+                                    </div>
+                                </td>
+                                <td>{{ $post_published->title }}</td>
+                                <td>{{ $post_published->slug }}</td>
+                                <td>{{ $post_published->created_at->format('d/m/Y à h:i') }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </section>
         </div>
-    </section>
+        <div class="tab-pane fade" id="archived-tab-pane" role="tabpanel" aria-labelledby="archived-tab" tabindex="0">
+            <section class="card">
+                <div class="card-body">
+                    <table id="archivedTable" class="display w-100">
+                        <thead>
+                        <tr>
+                            <th>Action</th>
+                            <th>Title</th>
+                            <th>Slug</th>
+                            <th>CreatedAt</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($posts_archived as $post_archived)
+                            <tr>
+                                <td>
+                                    <div class="d-flex gap-2">
+                                        <a href="{{ route('blog.show', $post_archived->slug) }}"
+                                           class="btn btn-sm btn-primary">
+                                            Show
+                                        </a>
+                                        <a href="{{ route('admin.blog.edit', $post_archived) }}"
+                                           class="btn btn-sm btn-warning">
+                                            Edit
+                                        </a>
+                                    </div>
+                                </td>
+                                <td>{{ $post_archived->title }}</td>
+                                <td>{{ $post_archived->slug }}</td>
+                                <td>{{ $post_archived->created_at->format('d/m/Y à h:i') }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </div>
+    </div>
 @endsection
 
 @push('scripts')
     <script src="https://cdn.datatables.net/2.0.5/js/dataTables.js"></script>
     <script>
-        let table = new DataTable('#myTable');
+        new DataTable('#draftTable');
+        new DataTable('#publishedTable');
+        new DataTable('#archivedTable');
     </script>
 @endpush
